41741039e1facb5a7688de6e3df7d3a413f4ce21
Amir <43443588+amirohn@users.noreply.github.com>
2022-05-14 22:02:53 +1000

    setting up global with mui (#1)

---
+++ pages/sign-up-form.js
@@ -0,0 +1,12 @@
+import {Container} from "@mui/material";
+import {SignUpForm} from "../component/signUpForm/index";
+
+const SignUp = () => {
+  return (
+    <Container maxWidth="md">
+      <SignUpForm />
+    </Container>
+  );
+};
+
+export default SignUp;
---
+++ styles/global/styles.js
@@ -0,0 +1,13 @@
+import {themeProvider, createMuiTheme} from "@mui/material/styles";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
