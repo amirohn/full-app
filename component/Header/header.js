512e8329a323c3cde4482849c3137ae6f9babd2e
Amir <43443588+amirohn@users.noreply.github.com>
2022-06-28 23:00:13 +1000

    header organised (#6)

--- component/Header/header.js
+++ component/Header/header.js
@@ -1,14 +1,63 @@
-import {Link, Button, Box} from "@mui/material";
+import {
+  Link,
+  Button,
+  Box,
+  Container,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  List,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import Image from "next/image";
+import logoImage from "../../asset/logo.png";
 
 export const Header = () => {
   return (
-    <>
-      <Box>
-        <Link href="/sign-up-form">sign up for free</Link>
+    <Container maxWidth="lg">
+      <Box sx={{display: "flex", justifyContent: "space-between"}}>
+        <Button>
+          <Link href="/">
+            <Image src={logoImage} alt="logo" />
+          </Link>
+        </Button>
+        <List
+          sx={{
+            width: "60%",
+            bgcolor: "background.paper",
+            display: "flex",
+            flexDirection: "row",
+          }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+        >
+          <ListItem>
+            <Button href="/">
+              <ListItemText primary="Home" />
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button href="/products">
+              <ListItemText primary="Shop" />
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button href="/sign-up-form">
+              <ListItemText primary="Login" />
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button href="/about">
+              <ListItemText primary="About" />
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button>
+              <ListItemText primary="Login" />
+            </Button>
+          </ListItem>
+        </List>
       </Box>
-      <Button variant="contained" mx={{color: "#ffffff"}}>
-        <Link href="/products">start shopping</Link>
-      </Button>
-    </>
+    </Container>
   );
 };
